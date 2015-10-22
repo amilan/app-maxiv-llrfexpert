6bbd77d27433cd369876acc11085b7d9385862f1
Antonio Milan Otero <antonio.milan_otero@maxlab.lu.se>
2015-10-22 13:22:03 +0200

    New widgets added.
    New widgets added. They are needed for the simplified version of the
    llrf gui.

---
+++ src/llrfgui/widgets/manualtuningsimple/manualtuningsimple.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+
+###############################################################################
+#     ManualTuningSimple is a widget used for the LLRF Expert GUI.
+#
+#     Copyright (C) 2013  Max IV Laboratory, Lund Sweden
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see [http://www.gnu.org/licenses/].
+###############################################################################
+
+"""
+ManualTuningSimple is a widget used for the LLRF Expert GUI.
+"""
+
+__all__ = ['ManualTuningSimple']
+
+__author__ = "amilan"
+
+__docformat__ = 'restructuredtext'
+
+import PyTango
+
+from taurus.external.qt import Qt, QtCore
+from taurus.qt.qtgui.util.ui import UILoadable
+
+from llrfgui.utils.decorators import alert_problems
+from llrfgui.widgets.basellrfwidget import BaseLLRFWidget
+
+
+@UILoadable(with_ui='ui')
+class ManualTuningSimple(BaseLLRFWidget):
+
+    def __init__(self, parent=None):
+        config_file = self._get_config_file_name(__file__)
+        BaseLLRFWidget.__init__(self, config_file, parent)
+        self.loadUi()
+
+    @alert_problems
+    def connect_with_devices(self):
+        """This method creates the tango device proxys. """
+
+        self._device_proxy = PyTango.DeviceProxy(self._device_name)
+
+    @alert_problems
+    def connect_signals(self):
+        QtCore.QObject.connect(self.ui.pushButton_rTU,
+                               QtCore.SIGNAL("clicked()"),
+                               self.tuning_resetA)
+        QtCore.QObject.connect(self.ui.pushButton_rTU_3,
+                               QtCore.SIGNAL("clicked()"),
+                               self.tuning_resetB)
+
+    @alert_problems
+    def tuning_resetA(self):
+        self._device_proxy['TuningResetA'] = True
+        self._device_proxy['TuningResetA'] = False
+
+    @alert_problems
+    def tuning_resetB(self):
+        self._device_proxy['TuningResetB'] = True
+        self._device_proxy['TuningResetB'] = False
+
+def main():
+    import sys
+    from taurus.qt.qtgui.application import TaurusApplication
+
+    app = TaurusApplication()
+    panel = ManualTuningSimple()
+    model = 'ws/rf/pynutaq_1'
+    panel.setModel(model)
+    panel.show()
+
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
